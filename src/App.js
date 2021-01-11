10b3f766f0d9fe037f19276bd1b2f81852ab48b8
niallrox <maximrbch@niuitmo.ru>
2021-01-11 23:31:34 +0300

    aa

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import './components/Header'
+import React, {Component} from "react";
+import {
+  BrowserRouter,
+  Switch,
+  Route,
+} from "react-router-dom";
+import MainPage from "./components/MainPage";
+import Login from "./components/Login";
+import PublicRoute from "./security/PublicRoute";
+import ProtectedRoute from "./security/ProtectedRoute";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+        <BrowserRouter>
+          <div>
+            <Switch>
+                <Route exact path="/" component={MainPage}/>
+                <Route exact path="/login" component={Login}/>
+            </Switch>
+          </div>
+        </BrowserRouter>
+    );
+  }
 }
 
-export default App;
+export default App
